8e8f69db14b6b7afb89fc2be0b17f4e54e1598fa
Gilberth Gabriel <dev.gilberth@gmail.com>
2024-06-05 03:24:52 -0300

    create products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomersModule } from './modules/customers/customers.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), CustomersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    CustomersModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
